c24fd47b7427ed7c8069c45d3d50bd46b6367bdc
Daniel Engvall <daniel.engvall@amelia.com>
2024-10-10 19:05:44 +0200

    Polishing the update readme script further

--- update_readme.py
+++ update_readme.py
@@ -1,16 +1,24 @@
 from datetime import datetime
 from jinja2 import Environment, FileSystemLoader
+from count_code_lines import repos_summary, OutputFormat
+import statistics
 
 
 def main(): 
     updated_at = datetime.now().strftime('%Y-%m-%d %H:%M')
 
-    # Set up Jinja2 environment
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('README.md.j2')
 
-    # Render and merge templates
-    rendered_readme = template.render(updated_at=updated_at)
+    x = repos_summary(['engdan77'], output_format=OutputFormat.JSON)
+    all_repos = [_['lines_of_code'] for _ in x['engdan77']]
+    d = {
+            'mean_across_repos': statistics.mean(all_repos),
+            'max_across_repos': max(all_repos),
+            'repos_count': len(x)
+        }
+
+    rendered_readme = template.render(updated_at=updated_at, d=d)
 
     # Save to README.md
     with open("README.md", "w") as f:
